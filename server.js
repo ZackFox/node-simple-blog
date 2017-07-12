c432671fbde33ea16c7b4c010969812ed0762f93
Segey <firstthinker1989@gmail.com>
2017-07-12 12:56:05 +0500

    upd

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 let express = require("express");
 let path = require("path");
 let bodyParser = require("body-parser");
+let hbs = require("express-handlebars");
 
 // var index = require("./routes/index");
 let taskRoutes = require("./routes/tasksRoutes");
 let app = express();
 
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
 app.set('views',path.join(__dirname,'views'));
-app.set('view engine','pug');
+app.set('view engine','hbs');
+app.engine('hbs',hbs({extname:'hbs'}));
 
 app.set('port', (process.env.PORT || 5000));
 
 app.get("/",(req,res)=>{
-    res.render('index',{title:"welcome", message:"hello world!"});
+    res.render('index',{msg:"hello world!"});
 });
 
 app.use(taskRoutes);
