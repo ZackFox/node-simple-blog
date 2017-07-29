ed7ca07fe212727eba0577bdb45e98ea251f8aa4
Segey <firstthinker1989@gmail.com>
2017-07-29 16:41:30 +0500

    comment fix

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -9,7 +9,6 @@ profileController.getProfilePage = (req, res, next) =>{
         .then((user) => {
             if(!user) return next();
             else {
-                console.log(user);
                 if(req.session.user && req.session.user.nickname == req.params.nickname )
                   res.locals.isOwn = true;
                 res.render("profile", { user: user, token: req.csrfToken()}); 
