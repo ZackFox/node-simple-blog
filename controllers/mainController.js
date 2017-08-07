798822b55c8fdd8cd1d4c0b24deb4cf31ec1a359
Segey <firstthinker1989@gmail.com>
2017-08-08 02:03:55 +0500

    pages

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -4,16 +4,16 @@ const Post = require("../model/postModel");
 const mainController = {};
 
 mainController.welcome = (req, res, next) => {
-  const page = req.query.page ? req.query.page : 1;
+  let page = req.query.page ? req.query.page - 1 : 0;
   const limit = 4;
 
   Post.find({}).count().then((postsQuantity) => {
     Post.find({})
-      .skip(limit * (page - 1))
+      .skip(limit * page)
       .limit(limit)
       .sort({ createTime: "desc" })
       .then((posts) => {
-        res.render("index", { token: req.csrfToken(), posts, postsQuantity, limit });
+        res.render("index", { token: req.csrfToken(), posts, limit, page, pages: Math.ceil(postsQuantity / limit) });
       });
   }).catch(err => next(err));
 };
