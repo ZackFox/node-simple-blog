8e0127ce63f250e398279e261b8c1a33fd3175c3
Segey <firstthinker1989@gmail.com>
2017-08-07 16:23:57 +0500

    pages v1

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -4,9 +4,19 @@ const Post = require("../model/postModel");
 const mainController = {};
 
 mainController.welcome = (req, res, next) => {
-  Post.find({}).then((posts) => {
-    res.locals.dateFormat = new Intl.DateTimeFormat("ru", { year: "2-digit", month: "numeric", day: "numeric" });
-    res.render("index", { token: req.csrfToken(), posts });
+  const page = req.query.page ? req.query.page : 1;
+  const limit = 4;
+
+  Post.find({}).count().then((postsQuantity) => {
+    Post.find({})
+      .skip(limit * (page - 1))
+      .limit(limit)
+      .sort({ createTime: "desc" })
+      .then((posts) => {
+        res.render("index", { token: req.csrfToken(), posts, postsQuantity, limit });
+      });
   }).catch(err => next(err));
 };
+
 module.exports = mainController;
+
